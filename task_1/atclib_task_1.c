3431359c33d03beadd9954bd962227d1f3c3b7b7
Atul R. Raut <atulraut17@gmail.com>
2017-08-22 22:45:43 -0700

    first commit

---
+++ task_1/atclib_task_1.c
@@ -0,0 +1,28 @@
+/*
+ * Aim  : Create Hello World Module
+ * Date : March-2-2017
+ * File : atclib_task1.c
+ * Author : atclib
+*/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int atclib_init(void)
+{
+	printk(KERN_ERR "[atclib2] : Hello World !\n");
+	return 0;
+}
+
+static void atclib_exit(void)
+{
+	printk(KERN_ERR "[atclib] : Dieing atclib !\n");
+}
+
+module_init(atclib_init);
+module_exit(atclib_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("894e26c7d25d");
+MODULE_DESCRIPTION("atclib task-1");
---
+++ task_2/README.txt
@@ -0,0 +1,11 @@
+1] Get local config file : 
+cp /boot/config-4.2.0-19-generic .config
+2] Download Linux Kernel Tree : 
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/
+
+
+Back Up : Ubuntu Grub -
+https://askubuntu.com/questions/216398/set-older-kernel-as-default-grub-entry/216420#216420
+Kernel Build : 
+https://kernelnewbies.org/KernelBuild
+https://askubuntu.com/questions/708937/autoconf-kernel-need-config-for-local-build
