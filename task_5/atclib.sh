18f88f09e433cc9355479efcd1b95c7445e85a21
Atul Raut <atul.droid@gmail.com>
2017-11-11 20:48:39 -0800

    task 5: Load USB module when insert keyboard
    When any USB keyboard is plugged in, the module will be
    automatically loaded by the correct userspace hotplug tools
    
    Signed-off-by: Atul Raut <atul.droid@gmail.com>

--- task_5/atclib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo HELLO ATUL! > /home/atul/dev/edu_task/task_5/udev_test_log.txt
-
-sudo insmod /home/atul/dev/edu_task/task_5/atclib_task_5.ko
-
--- task_5/atclib_task_5.c
+++ task_5/atclib_task_5.c
@@ -1,6 +1,9 @@
 /*
- * Aim  : Create Hello World Module
- * Date : July-30-2017
+ * Aim  : When any USB keyboard is plugged in, the module will be
+ *        automatically loaded by the correct userspace hotplug tools (which
+ *        are implemented by depmod / kmod / udev / mdev / systemd, depending
+ *        on what distro you are using.)
+ * Date : Sept-24-2017
  * File : atclib_task_5.c
  * Author : atclib
  */
@@ -11,44 +14,35 @@
 #include <linux/usb.h>
 #include <linux/hid.h>
 
-static int atclib_probe(struct usb_interface *interface,
-		const struct usb_device_id *id)
-{
-	pr_debug("[atclib-5] :USB keyboard plugged in !\n");
-	return 0;
-}
-
-static void atclib_disconnect(struct usb_interface *interface)
-{
-	pr_debug("[atclib-5] USB keyboard disconnected.\n");
-}
-
+/* the driver registers its driver object with the USB subsystem and
+ * later uses vendor and device identifiers to tell if tis hardware has
+ * been installed. The struct usb_device_id structure provided a list
+ * of different types of USB devices that htis driver supports.
+ * Trick: Pointer to the struct usb_device_id table that contrains
+ * a list of all of the different kinds of USB devices this driver can accept.
+ * If this variable is not set, the probe function callback int the USB
+ * driver is never called. If you want your driver always to be called
+ * for everyUSB device in the system, create a entry that sets only the
+ * driver_info field.
+ */
 static const struct usb_device_id id_table[] = {
-	{USB_INTERFACE_INFO
-	(USB_INTERFACE_CLASS_HID, USB_INTERFACE_SUBCLASS_BOOT,
-	USB_INTERFACE_PROTOCOL_KEYBOARD)},
-	{},
+		{USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
+		USB_INTERFACE_SUBCLASS_BOOT,
+		USB_INTERFACE_PROTOCOL_KEYBOARD)},
+		{}
 };
 
 MODULE_DEVICE_TABLE(usb, id_table);
 
-static struct usb_driver atclib_driver = {
-	.name = "atclib-usb",
-	.probe = atclib_probe,
-	.disconnect = atclib_disconnect,
-	.id_table = id_table,
-};
-
 static int __init atclib_init(void)
 {
 	pr_debug("[atclib-5] : atclib init called. !\n");
-	return usb_register(&atclib_driver);
+	return 0;
 }
 
 static void atclib_exit(void)
 {
 	pr_debug("[atclib-5] : Dieing usb atclib !\n");
-	usb_deregister(&atclib_driver);
 }
 
 module_init(atclib_init);
